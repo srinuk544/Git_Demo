b3fb87ae76b957c1cb6a7f20d6e29bdb059fe993
srinu544 <kattasrinu544@gmail.com>
2025-12-15 16:43:30 +0530

    To perform some Sleep method

--- src/com/test/Testclass.java
+++ src/com/test/Testclass.java
@@ -5,6 +5,12 @@ public class Testclass extends Thread {
 	public void run() {
 		for (int i = 0; i <= 5; i++) {
 			if (i % 2 == 0) {
+				try {
+					Thread.sleep(2500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 				System.out.println("even numbers from run method:" + i);
 
 			}
@@ -12,12 +18,14 @@ public class Testclass extends Thread {
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		Testclass t = new Testclass();
 		t.start();
+
 		for (int i = 1; i <= 8; i++) {
-			if (i % 2!= 0) {
+			if (i % 2 != 0) {
+				Thread.sleep(1000);
 				System.out.println("odd numbers from Main method:" + i);
 			}
 
